290067ea8bce04c88df39dc52d3bfc444ce01731
Domonique Griffin <griffind2009@gmail.com>
2016-10-09 15:29:06 -0400

    can create new articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,8 +1,16 @@
 class ArticlesController < ApplicationController
   def new
-    # @articles = Artiles.new
   end
   def create
-    render plain: params[:article].inspect
+@article = Article.create(article_params)
+redirect_to @article
   end
+  def show
+    @article = Article.find(params[:id])
+  end
+end
+
+private
+def article_params
+  params.require(:article).permit(:title, :text)
 end
